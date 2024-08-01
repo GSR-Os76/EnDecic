4ccf1bf11a098ce4246b5f5564ff64520ac97396
hmJ4Zor-Q-3 <129763409+hmJ4Zor-Q-3@users.noreply.github.com>
2024-08-01 16:53:30 -0400

    Remove lingering GSR.Utilic clone files.

--- Utilic/Generics/Queue/IQueueExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GSR.Utilic.Generic
-{
-    public static class IQueueExtensions
-    {
-        public static T[] DequeueAll<T>(this IQueue<T> q) => q.Dequeue(q.Count);
-    } // end class
-} // end namespace
